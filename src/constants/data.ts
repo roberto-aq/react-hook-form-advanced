6d262b80d9681d0cfa0b289ed8673a071787182b
roberto-aq <baxro2000@gmail.com>
2025-02-24 10:08:53 -0500

    Añadida la data

---
+++ src/constants/data.ts
@@ -0,0 +1,34 @@
+/* Multiselect */
+export const channels = [
+	{ value: 'facebook', label: 'Facebook' },
+	{ value: 'instagram', label: 'Instagram' },
+	{ value: 'twitter', label: 'Twitter' },
+	{ value: 'linkedin', label: 'LinkedIn' },
+	{ value: 'youtube', label: 'YouTube' },
+	{ value: 'tiktok', label: 'TikTok' },
+	{ value: 'google_ads', label: 'Google Ads' },
+];
+
+/* Single Selects */
+export const customersOptions = [
+	{ value: 'all_customers', label: 'Todos los clientes' },
+	{ value: 'professionals', label: 'Profesionales' },
+	{ value: 'companies', label: 'Empresas' },
+	{ value: 'individuals', label: 'Individuos' },
+	{ value: 'students', label: 'Estudiantes' },
+	{ value: 'family', label: 'Familia' },
+];
+
+export const rulesOptions = [
+	{ value: 'age', label: 'Edad' },
+	{ value: 'location', label: 'Ubicación' },
+	{ value: 'purchases', label: 'Compras realizadas' },
+	{ value: 'spent', label: 'Monto gastado' },
+	{ value: 'frequency', label: 'Frecuencia de compra' },
+];
+
+export const operators = [
+	{ value: '=', label: '=' },
+	{ value: '>', label: '>' },
+	{ value: '<', label: '<' },
+];
